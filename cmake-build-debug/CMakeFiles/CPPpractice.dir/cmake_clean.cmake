834c5ee05ea35e91f9914ea09f5372d086ca21eb
Afish716 <1409152793@qq.com>
2020-12-02 20:25:57 +0800

    初始提交

---
+++ cmake-build-debug/CMakeFiles/CPPpractice.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPPpractice.dir/main.cpp.obj"
+  "CPPpractice.exe"
+  "CPPpractice.exe.manifest"
+  "CPPpractice.pdb"
+  "libCPPpractice.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPPpractice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
