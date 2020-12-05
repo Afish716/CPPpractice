4511a0ce617b4a91e33d1664f5b96b2e5f769533
Afish716 <1409152793@qq.com>
2020-12-05 17:25:10 +0800

    2020-12-05 17:23:33

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include "Practice3.h"
 #include "Practice6.h"
 int main() {
-      string s1,s2;
-      cin>>s1;
-      cin>>s2;
-      bool flag=practice6_17_1(s1);
-      cout<<flag<<'\n' ;
-      practice6_17_2(s2);
-      cout<<s2;
+   vector<string> v2={"A1","B2","C3"};
+   practice6_33(v2,0);
 }
