a37612eec3d2c25c556237818bdce159551de355
Afish716 <1409152793@qq.com>
2020-12-05 17:23:45 +0800

    2020-12-05 17:23:33

--- Practice6.cpp
+++ Practice6.cpp
@@ -37,3 +37,25 @@ string practice6_17_2(string& string1){
         }
     }
 }
+
+int practice6_21(int x,int* ptr){
+    return x>*ptr? x:*ptr;
+}
+
+int practice6_22(int* &ptr1,int* &ptr2){
+    int* ptr3=ptr1;
+    ptr1=ptr2;
+    ptr2=ptr3;
+}
+
+int practice6_33(const vector<string>& v1,int x){
+    static int size=v1.size();
+    static int count=0;
+    if (count==size){
+
+    } else{
+        cout<<v1[count]<<" ";
+        count++;
+        practice6_33(v1,count);
+    }
+}//const表面我不希望改变vector里面的值 &表示不希望牺牲效率来传入拷贝
--- Practice6.h
+++ Practice6.h
@@ -15,3 +15,6 @@ int practice6_7();
 int practice6_10(int* x1,int* x2);
 bool practice6_17_1(const string& string1);
 string practice6_17_2(string& string1);
+int practice6_21(int x,int* ptr);
+int practice6_22(int* &ptr1,int* &ptr2);
+int practice6_33(const vector<string>& v1,int x);
